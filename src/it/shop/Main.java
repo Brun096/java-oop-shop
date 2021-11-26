772c891f56eb4965a8eb26e97e33439fabcf454d
Brun096 <Brunoh96@outlook.it>
2021-11-26 14:25:53 +0100

    m

--- src/it/shop/Main.java
+++ src/it/shop/Main.java
@@ -4,15 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Shop shop=new Shop();
+		
+		Shop shop=new Shop("Balsamo","Cura i capelli",30,22);
+		
+		System.out.println("il nome del prodotto è "+shop.getNome());
+		System.out.println("il prezzo senza iva è "+shop.getPrezzo());
+		System.out.println("il prezzo pieno con iva è "+shop.getPrezzoivato());
+		System.out.println("il codice del prodotto è "+shop.getCodice());
+		System.out.println("il codice-nome del prodotto è "+shop.getNomeEsteso());
 		
 		
-		System.out.println(shop.getNome());
-		System.out.println("il codice è: "+shop.getCodice());
-		System.out.println("il prezzo senza iva è: "+shop.getPrezzo());
-		System.out.println("prezzo con iva "+shop.getPrezzoPieno());
-		System.out.println("descrizione del prodotto: "+shop.getDescrizione());
-		System.out.println("il codice-nome del prodotto è: "+shop.getNomePiuCodice());
 	}
 
 }
